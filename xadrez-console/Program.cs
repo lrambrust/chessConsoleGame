ab282a6c3cace8b14eeeed4a2bf87fb901c6c831
Lucas Ambrust <ambrust.lucas@gmail.com>
2019-04-06 16:05:26 -0300

    Implementando tela com o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -10,12 +7,11 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();
         }
     }
 }
